d39768b06dfb6b386c2e8c6c840464efca04cede
= <=>
2025-11-29 23:51:18 +0900

    Add StartServer, StopServer, RebootServer, ForceShutdownServer.

--- compute_v3.go
+++ compute_v3.go
@@ -2,6 +2,8 @@ package conoha
 
 import (
 	"encoding/json"
+	"fmt"
+	"strings"
 
 	"github.com/elfincafe/annette"
 )
@@ -29,10 +31,99 @@ func (api *V3) GetServers() (*GetServersResponse, error) {
 	if err != nil {
 		return nil, err
 	}
+	if !res.IsStatus200() {
+		var v ConohaError
+		json.Unmarshal(res.Binary(), &v)
+		return nil, fmt.Errorf("status:%d, error:%s", v.Code, v.Error)
+	}
 	var v GetServersResponse
 	err = json.Unmarshal(res.Binary(), &v)
 	if err != nil {
 		return nil, err
 	}
 	return &v, nil
+}
+
+func (api *V3) StartServer(serverId string) error {
+	endpoint := api.Endpoints.Compute
+	endpoint.Path = fmt.Sprintf(`/v2.1/servers/%s/action`, serverId)
+	body := `{
+		"os-start": null
+	}`
+	client := annette.New(endpoint)
+	client.Header.Set("Content-Type", "application/json")
+	client.Header.Set("X-Auth-Token", api.Token)
+	res, err := client.Post(strings.NewReader(body))
+	if err != nil {
+		return err
+	}
+	if !res.IsStatus202() {
+		var v ConohaError
+		json.Unmarshal(res.Binary(), &v)
+		return fmt.Errorf("status:%d, error:%s", v.Code, v.Error)
+	}
+	return nil
+}
+
+func (api *V3) StopServer(serverId string) error {
+	endpoint := api.Endpoints.Compute
+	endpoint.Path = fmt.Sprintf(`/v2.1/servers/%s/action`, serverId)
+	body := `{
+		"os-stop": null
+	}`
+	client := annette.New(endpoint)
+	client.Header.Set("Content-Type", "application/json")
+	client.Header.Set("X-Auth-Token", api.Token)
+	res, err := client.Post(strings.NewReader(body))
+	if err != nil {
+		return err
+	}
+	if !res.IsStatus202() {
+		var v ConohaError
+		json.Unmarshal(res.Binary(), &v)
+		return fmt.Errorf("status:%d, error:%s", v.Code, v.Error)
+	}
+	return nil
+}
+
+func (api *V3) RebootServer(serverId string) error {
+	endpoint := api.Endpoints.Compute
+	endpoint.Path = fmt.Sprintf(`/v2.1/servers/%s/action`, serverId)
+	body := `{
+		"reboot": {"type": "SOFT"}
+	}`
+	client := annette.New(endpoint)
+	client.Header.Set("Content-Type", "application/json")
+	client.Header.Set("X-Auth-Token", api.Token)
+	res, err := client.Post(strings.NewReader(body))
+	if err != nil {
+		return err
+	}
+	if !res.IsStatus202() {
+		var v ConohaError
+		json.Unmarshal(res.Binary(), &v)
+		return fmt.Errorf("status:%d, error:%s", v.Code, v.Error)
+	}
+	return nil
+}
+
+func (api *V3) ForceShutdownServer(serverId string) error {
+	endpoint := api.Endpoints.Compute
+	endpoint.Path = fmt.Sprintf(`/v2.1/servers/%s/action`, serverId)
+	body := `{
+		"os-stop": {"force_shutdown": true}
+	}`
+	client := annette.New(endpoint)
+	client.Header.Set("Content-Type", "application/json")
+	client.Header.Set("X-Auth-Token", api.Token)
+	res, err := client.Post(strings.NewReader(body))
+	if err != nil {
+		return err
+	}
+	if !res.IsStatus202() {
+		var v ConohaError
+		json.Unmarshal(res.Binary(), &v)
+		return fmt.Errorf("status:%d, error:%s", v.Code, v.Error)
+	}
+	return nil
 }
